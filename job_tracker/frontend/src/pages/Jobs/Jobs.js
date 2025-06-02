d8c64b460ad41e2c0a20226b2de3649d692c2d47
David Fine <davidfine@Davids-MacBook-Pro.local>
2025-06-02 13:10:00 -0400

    checkpoint

--- job_tracker/frontend/src/pages/Jobs/Jobs.js
+++ job_tracker/frontend/src/pages/Jobs/Jobs.js
@@ -2,44 +2,45 @@
 
 // Importing modules
 import { useState, useEffect } from "react";
-import "./styles/Jobs.css";
-import JobsTable from "./components/JobTable";
-import Box from "@mui/material/Box";
+
 import styled from "@emotion/styled";
+import JobsTable from "./components/JobTable";
+import { Grid } from "@mui/material";
 
-const StyledBox = styled(Box)(({theme}) => ({
-          backgroundColor: "#fff",
-          border: "3px inset #e0e0e0",
-          borderRadius: "12px",
-          boxShadow: "3px 10px 10px hsla(0, 71.40%, 1.40%, 0.61)",
-          padding: 4,
-          display: "flex",
+const StyledGrid = styled(Grid)(({ theme }) => ({
+  backgroundColor: "#fff",
+  border: "3px inset #e0e0e0",
+  borderRadius: "12px",
+  boxShadow: "3px 10px 10px hsla(0, 71.40%, 1.40%, 0.61)",
+  padding: 4,
+  display: "flex",
 
-          overflow: "auto",
-          fontFamily: "Arial, sans-serif",
+  overflow: "auto",
+  fontFamily: "Arial, sans-serif",
 }));
 
 
+
 function Jobs() {
-    const [jobs, setJobs] = useState([]);
+  const [jobs, setJobs] = useState([]);
   useEffect(() => {
-      fetch("/jobs")
-          .then((res) => {
-              return res.json()
-          })
-          .then((jobs) => {
-            console.log(jobs);
-            setJobs(jobs);
-          });
+    fetch("/jobs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((jobs) => {
+        console.log(jobs);
+        setJobs(jobs);
+      });
   }, []);
 
-    return (
-      <div className="Jobs">
-        <StyledBox>
-          <JobsTable jobs={jobs}></JobsTable>
-        </StyledBox>
-      </div>
-    );
+  return (
+    <StyledGrid container spacing={2}>
+      <Grid sx={{ padding: "8px" }}>
+        <JobsTable jobs={jobs}></JobsTable>
+      </Grid>
+    </StyledGrid>
+  );
 }
 
 export default Jobs;
--- job_tracker/frontend/src/pages/Jobs/components/JobTable.js
+++ job_tracker/frontend/src/pages/Jobs/components/JobTable.js
@@ -1,62 +1,90 @@
 import * as React from "react";
-import styled from '@emotion/styled'
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import styled from "@emotion/styled";
 
+import { Box } from "@mui/material";
 
-function JobsTableHeaderRow(){
+import { DataGrid } from "@mui/x-data-grid";
+
+function JobsTable({ jobs }) {
+  const columns = [
+    { field: "company", headerName: "Company", width: 90 },
+    {
+      field: "title",
+      headerName: "Title",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "compensation",
+      headerName: "Compensation",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "workModel",
+      headerName: "Work Model",
+      type: "number",
+      width: 110,
+      editable: true,
+    },
+    {
+      field: "location",
+      headerName: "Location",
+      width: 160,
+    },
+    {
+      field: "source",
+      headerName: "Source",
+      width: 160,
+    },
+    {
+      field: "applicationStatus",
+      headerName: "Application Status",
+      width: 160,
+    },
+    {
+      field: "resume",
+      headerName: "Resume",
+      width: 160,
+    },
+    {
+      field: "coverLetter",
+      headerName: "Cover Letter",
+      width: 160,
+    },
+  ];
+  const rows = jobs.map((_job, index) => {
+    const job = JSON.parse(_job);
+    console.log(job);
+    return {
+      id: index,
+      company: job.company_name,
+      title: job.title,
+      compensation: `$${job.compensation_low}-$${job.compensation_high}`,
+      workModel: job.work_model,
+      location: job.location,
+      source: job.source,
+      applicationStatus: job.application_status,
+    };
+  });
+
   return (
-    <TableRow>
-      <TableCell align="center">Company</TableCell>
-      <TableCell align="center">Title</TableCell>
-      <TableCell align="center">Compensation</TableCell>
-      <TableCell align="center">Work Model</TableCell>
-      <TableCell align="center">Location</TableCell>
-      <TableCell align="center">Source</TableCell>
-      <TableCell align="center">Application Status</TableCell>
-      <TableCell align="center">Resume</TableCell>
-      <TableCell align="center">Cover Letter</TableCell>
-    </TableRow>
-  );
-}
-
-function JobsTableRow({job}) {
-  const tableCells = Object.values(job).map(value => { return <TableCell align="right">{value}</TableCell>})
-  return (
-    <TableRow>
-      <TableCell align="right">{job.company_name}</TableCell>
-      <TableCell align="right">{job.title}</TableCell>
-      <TableCell align="right">{`$${job.compensation_low} - $${job.compensation_high}`}</TableCell>
-      <TableCell align="right">{job.work_model}</TableCell>
-      <TableCell align="right">{job.location}</TableCell>
-      <TableCell align="right">{job.posting_source}</TableCell>
-      <TableCell align="right">{job.application_status}</TableCell>
-      <TableCell align="right"></TableCell>
-      <TableCell align="right"></TableCell>
-    </TableRow>
-  );
-
-}
-function JobsTable({jobs}) {
-  return (
-    <TableContainer component={Paper} sx={{ maxWidth: '100%'}}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <JobsTableHeaderRow />
-        </TableHead>
-        <TableBody>
-          {jobs.map((jobStr) => {
-            const job = JSON.parse(jobStr); // safely parse here
-            return <JobsTableRow job={job} key={job.id} />;
-          })}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box container sx={{ height: 400, width: 1500 }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5]}
+        checkboxSelection
+        disableRowSelectionOnClick
+      />
+    </Box>
   );
 }
 
